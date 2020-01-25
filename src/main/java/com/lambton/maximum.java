836630013daf9dabefc42327ab1489425a0b199d
manbeergil <manbeergill545@gmail.com>
2020-01-24 23:52:48 -0500

    sum and avg

---
+++ src/main/java/com/lambton/maximum.java
@@ -0,0 +1,19 @@
+package com.lambton;
+
+public class maximum {
+    public static void main(String[] args) {
+        int a[];
+        a = new int[]{3,4,6,1,9};
+        int t =0;
+        for(int i = 0;i<a.length;i++)
+        {
+            int b=a[i];
+            t =t+ b;
+
+        }
+        System.out.println("sum ="+t);
+        int c = a.length;
+        System.out.println("avg ="+t/c);
+
+    }
+}
